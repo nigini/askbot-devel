c732cc389a44684b488ec564c3ae3a9f405d53d2
Nigini Oliveira <nigini@gmail.com>
2019-02-04 16:29:56 -0800

    Added TheWildLab Project!

---
+++ askbot/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('askbot', '0013_auto_20180922_1908'),
+        ('askbot', '0014_populate_askbot_roles'),
+    ]
+
+    operations = [
+    ]
